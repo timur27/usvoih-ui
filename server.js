d7f61f1f28b26a3a4fce09a90634d0b344c36ea5
Timur Karimov <timur.karimow.95@gmail.com>
2020-05-03 20:00:18 +0200

    All fixes to deploy sucessfully

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('./dist/usvoih-ui'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/usvoih-ui/'}
+  res.sendFile('app/app.component.html', {root: 'dist/usvoih-ui/'}
 );
 });
 app.listen(process.env.PORT || 8080);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SpotComponent} from './spot/spot.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SpotService} from './spot/spot.service';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpotComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    SpotService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
